12048fcfdb00284bcf9e87a906186628dece5d8c
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-23 18:32:20 +0530

    init commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using B2Me.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B2Me.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
